22cc3c3d92e26b26022fadb971f26c898c7b64d7
ScorpionInc <d2dejohn@gmail.com>
2024-05-12 22:47:01 -0400

    Update .si_profile.sh

--- .si_profile.sh
+++ .si_profile.sh
@@ -7,5 +7,8 @@ echo "$temprepo $tempfile"
 rm -f "$tempfile"
 time git clone --no-checkout --depth 1 https://github.com/ScorpionInc/Bash-Sandbox.git "$temprepo"
 profileHashes=$(git -C "$temprepo" ls-tree --full-name --name-only -r HEAD | grep -E '^Profile[/\]+.*[.]sha512$')
-echo "$profileHashes"
+profileHash=$(echo "$profileHashes" | cut -c 9-)
+profileFile=${profileHash%.*}
+echo "$profileFile"
+curl "https://raw.githubusercontent.com/ScorpionInc/Bash-Sandbox/main/Profile/$profileHash"
 rm -rf "$temprepo"
