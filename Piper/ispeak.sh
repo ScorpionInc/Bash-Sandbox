55cb76a512c4248af52df22ff6c3e114a6addce5
ScorpionInc <d2dejohn@gmail.com>
2024-06-26 00:18:54 -0400

    Update ispeak.sh
    Now uses high quality model. Adds prompt and silences debug information.

--- Piper/ispeak.sh
+++ Piper/ispeak.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-voices=( ./piper_voices/*.onnx )
+voices=( ./piper_voices/*-high.onnx )
 echo "Using first voice at path: ${voices[0]}"
-while IFS=$'\n' read -r line; do
-    echo "$line" | ./piper --model "${voices[0]}" --output_raw | aplay -f S16_LE -c1 -r22050 -t raw -
+while IFS= read -rp "What do you want to say?: " line; do
+	[ -z "$line" ] && break;
+	(echo "$line" | ./piper --model "${voices[0]}" --output_raw 2>/dev/null | aplay -f S16_LE -c1 -r22050 -t raw - &>/dev/null 2>&1)&
 done
 
